6f7d259e6e0bfb71c2b7220e0b5c75c6599aeec4
trevorfaivre <trevorfaivre@hotmail.com>
2022-05-17 20:58:11 -0500

    export database to server.js

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
+
+const { response } = require('express');
+const drinks = require('./models/drinks');
+
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
 const port = 3000;
 
+
+
+app.get('/drinks', (request, response) => {
+    response.render('drinks_index.ejs', {drinks})
+});
+
+// app.get('/drinks', (request, response) => {
+//     response.render('index.ejs', {drinks});
+// });
 app.get('/', (request, response) => {
     response.send('Welcome to the GitPub App!');
 });
+
+
+app.get("/drinks", (request, response) => {
+    response.send("drinks")
+});
+
+console.log(drinks)
+
+// app.get('/drinks/:index', (request, response) => {
+//     response.render('index.ejs', {allDrinks: drinks});
+// });
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('Listening on 3000')
